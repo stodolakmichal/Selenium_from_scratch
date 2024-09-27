c53f64556d608ce281270a3d64883a2dea83e081
stodolakmichal <stodolak.michal@gmail.com>
2024-09-27 23:20:47 +0200

    Added basic test_default_checkboxes

--- tests/test_checkboxes.py
+++ tests/test_checkboxes.py
@@ -1,3 +1,12 @@
 from selenium.webdriver.common.by import By
 
-test_url = "https://the-internet.herokuapp.com/entry_ad"
+test_url = "https://the-internet.herokuapp.com/checkboxes"
+
+
+def test_default_checkboxes(driver):
+    driver.get(test_url)
+    checkboxes = driver.find_elements(By.XPATH, "//input[@type='checkbox']")
+    is_checkbox_0_checked = checkboxes[0].is_selected()
+    is_checkbox_1_checked = checkboxes[1].is_selected()
+    assert not is_checkbox_0_checked, "Checkbox 0 is checked, but shouldn't be!"
+    assert is_checkbox_1_checked, "Checkbox 1 isn't checked, but should be!"
