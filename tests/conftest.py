e1f8d9def17d70bd55787fbb761ac31337b727d7
stodolakmichal <stodolak.michal@gmail.com>
2024-09-28 20:18:36 +0200

    test_if_buttons_are_clickable added

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,7 @@ from selenium import webdriver
 @pytest.fixture()
 def driver():
     options = webdriver.ChromeOptions()
-    options.add_argument("--headless=new")
+    #options.add_argument("--headless=new")
     driver = webdriver.Chrome(options=options)
     driver.implicitly_wait(10)
     yield driver
--- tests/test_checkboxes.py
+++ tests/test_checkboxes.py
@@ -10,3 +10,15 @@ def test_default_checkboxes(driver):
     is_checkbox_1_checked = checkboxes[1].is_selected()
     assert not is_checkbox_0_checked, "Checkbox 0 is checked, but shouldn't be!"
     assert is_checkbox_1_checked, "Checkbox 1 isn't checked, but should be!"
+
+
+def test_if_buttons_are_clickable(driver):
+    driver.get(test_url)
+    checkboxes = driver.find_elements(By.XPATH, "//input[@type='checkbox']")
+    for index, checkbox in enumerate(checkboxes):
+        is_checkbox_selected = checkbox.is_selected()
+        checkbox.click()
+        if is_checkbox_selected:
+            assert not checkbox.is_selected(), f"Checkbox at {index} is not clickable"
+        else:
+            assert checkbox.is_selected(), "Checkbox at {index} is not clickable"
